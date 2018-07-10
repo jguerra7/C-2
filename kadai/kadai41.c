e81f948002b79101acef6d1f14dddf833b76c34c
kagaya <jm.kagaya@gmail.com>
2018-07-10 14:45:04 +0900

    Done

--- kadai/kadai41.c
+++ kadai/kadai41.c
@@ -1,7 +1,34 @@
-#include <stdio.h>
 
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 void main(void)
 {
 
+char str[100]="KaGaya           JOhN           MeDeL   koBa DenSha ";
+int c=0,j=0;
+while(str[c]!='\0'){
+    if(str[c]!=' '){
+        str[j++]=str[c];
+    }
+    c++;
+}
+str[j]='\0';
+printf("%s \n",str);
     
 }
+
+
+
+
+
+
+
