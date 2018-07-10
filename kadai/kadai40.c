a20c8e427d27fd17b80eef9244d6f952f7b36e26
kagaya <jm.kagaya@gmail.com>
2018-07-10 14:36:45 +0900

    Done

--- kadai/kadai40.c
+++ kadai/kadai40.c
@@ -4,12 +4,35 @@
 
 
 
-#include <stdio.h>
 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 void main(void)
 {
-
+char str[100] = "KaGaya JOhN MeDeL koBa DenSha ";
+char str2[] = "a";
+int count = 0;
+ 
+   
+   printf("%s \n",str);
+    for ( int i=0; str[i]!= '\0'; i++)
+    {
+        
+      if(str[i] == str2[0]){
+          count++;
+      }
+        
+    }
+    printf("%s = %d \n",str2,count);
+    
     
 }
+
+
+
+
 
 
