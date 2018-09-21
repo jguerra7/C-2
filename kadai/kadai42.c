59988da5bb6a35e507b64c0dc58087b0dc1c9850
kagaya <jm.kagaya@gmail.com>
2018-09-21 23:28:47 +0900

    Forget to save

--- kadai/kadai42.c
+++ kadai/kadai42.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 void main(void)
 {
 
-
+    int c;
+    FILE *fin;
+    fin = fopen("./test.txt", "r");
+    if (fin) {
+    while ((c = getc(fin)) != EOF)
+        putchar(c);
+    fclose(fin);
+}
+  
     
 }
